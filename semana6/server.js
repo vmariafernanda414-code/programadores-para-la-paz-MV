10277bfdc21f3c7fa802b15c66364499da744ba6
vmariafernanda414-code <vmariafernanda414@gmail.com>
2026-06-23 02:54:48 +0000

    Actividad semana 6

--- semana6/server.js
+++ semana6/server.js
@@ -16,6 +16,19 @@ app.post('/registro', (req, res) => {
 
 });
 
+app.post('/incidencia', (req, res) => {
+
+  const tipo = req.body.tipo;
+  const descripcion = req.body.descripcion;
+
+  res.json({
+    mensaje: "Incidencia registrada",
+    tipo: tipo,
+    descripcion: descripcion
+  });
+
+});
+
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
 });
